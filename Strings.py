dbf13a05dd0e0a722b95869b30c96c17b54ef170
patrick-kuenig <98701533+patrick-kuenig@users.noreply.github.com>
2024-04-28 10:47:55 +0300

    Update Strings.py
    Added spaces

--- Strings.py
+++ Strings.py
@@ -1,7 +1,7 @@
 string = "это моя строка"
-print("1." + string[0])
-print("2." + string[-1])
-print("3." + string[2:5])
-print("4." + string[::-1])
-print("5." + str(len(string)))
-print("6." + "это новая строка\t" + string)
+print("1. " + string[0])
+print("2. " + string[-1])
+print("3. " + string[2:5])
+print("4. " + string[::-1])
+print("5. " + str(len(string)))
+print("6. " + "это новая строка\t" + string)
