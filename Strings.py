73ef7c4ac6f89d85d0e63e8df640c7ffe48d68c4
patrick-kuenig <98701533+patrick-kuenig@users.noreply.github.com>
2024-04-28 10:45:46 +0300

    Update Strings.py
    The results should've been listed in order initially

--- Strings.py
+++ Strings.py
@@ -1,7 +1,7 @@
-str = "это моя строка"
-print(str[0])
-print(str[-1])
-print(str[2:5])
-print(str[::-1])
-print(len(str))
-print("это новая строка\n" + str)
+string = "это моя строка"
+print("1." + string[0])
+print("2." + string[-1])
+print("3." + string[2:5])
+print("4." + string[::-1])
+print("5." + str(len(string)))
+print("6." + "это новая строка\t" + string)
