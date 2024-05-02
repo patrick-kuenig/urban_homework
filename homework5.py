49f223446645a5b1c858a06e42ee13aec86bd4d1
patrick-kuenig <kuenig.patrick@gmail.com>
2024-05-02 14:47:44 +0300

    Homework4_01_05_2024

---
+++ homework5.py
@@ -0,0 +1,12 @@
+#lists
+my_list = ["apple", "banana", "pear", "pineapple", "orange", "beer" ]
+print(f"My list: {my_list}")
+print(f"First item in list: {my_list[0]}\nLast item in list: {my_list[-1]}")
+print(f"Third, fourth and fifth item in the list: {my_list[2:5]}")
+my_list[2] = "cucumber"
+print(f"Changed list: {my_list}")
+#dicts
+my_dict = {"deer": "Reh", "dog": "Hund", "house": "Haus", "crazy": "verrückt"}
+print(f"This is a dictionary: {my_dict}")
+my_dict["wall"] = "Mauer"
+print(f"Dictionary with added key/value pair: {my_dict}")
