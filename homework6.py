a7b1ff5799250d26b243b158662bc76b7078c6c4
patrick-kuenig <kuenig.patrick@gmail.com>
2024-05-04 10:04:54 +0300

    Homework4_01_05_2024

--- homework6.py
+++ homework6.py
@@ -3,5 +3,5 @@ def average_grade(grades, students):
     return
 
 
-if __name__ != "__main__":
+if __name__ == "__main__":
     average_grade([[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]], {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'})
