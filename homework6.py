b7e9391adc6975711cf929818494c4c33564170d
patrick-kuenig <kuenig.patrick@gmail.com>
2024-05-04 10:03:41 +0300

    Homework4_01_05_2024

---
+++ homework6.py
@@ -0,0 +1,7 @@
+def average_grade(grades, students):
+    print(dict(map(lambda x, y: (x, round(sum(y)/len(y), 1), ), sorted(students), grades)))
+    return
+
+
+if __name__ != "__main__":
+    average_grade([[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]], {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'})
