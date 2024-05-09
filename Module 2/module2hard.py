cdcfd52df0572ef0766162888b08279b23c395e4
patrick-kuenig <kuenig.patrick@gmail.com>
2024-05-09 17:45:43 +0300

    Added try/except

--- Module 2/module2hard.py
+++ Module 2/module2hard.py
@@ -7,7 +7,7 @@ def password_game(key):
         result_list = []
         for i in keys[:-1]:
             for j in keys[1:]:
-                if i + j == key:
+                if key % (i + j) == 0:
                     result_list.extend([i, j])
             keys.remove(i)
         result = ''.join(str(x) for x in result_list)
@@ -20,6 +20,11 @@ if __name__ == "__main__":
     try:
         input_ = int(input("Which number do you need the password for?\n"))
         print(f"Password for {input_}:", password_game(input_))
+        if input('Would you like to know all the passwords? (y/n)') == 'y':
+            for all in [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]:
+                print(f"{all}:", password_game(all))
+        else:
+            print('Goodbye')
 
     except ValueError:
         print("You entered an invalid value.")
