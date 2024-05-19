2018fe3f4f22ea3050ce2bb562af131b87909053
patrick-kuenig <kuenig.patrick@gmail.com>
2024-05-19 19:55:23 +0300

    homework_19_05_2024

---
+++ Module_3/module3_hard.py
@@ -0,0 +1,30 @@
+def calculate_structure_sum(*args):
+    result = 0
+    for arg in args:
+        if isinstance(arg, dict) and arg is not None:
+            for key, value in arg.items():
+                result += calculate_structure_sum(key)
+                result += calculate_structure_sum(value)
+        elif isinstance(arg, list) and len(arg) != 0:
+            result += calculate_structure_sum(*arg)
+        elif isinstance(arg, tuple) and len(arg) != 0:
+            result += calculate_structure_sum(*arg)
+        elif isinstance(arg, set) and len(arg) != 0:
+            result += calculate_structure_sum(*arg)
+        elif isinstance(arg, int):
+            result += arg
+        elif isinstance(arg, str):
+            result += len(arg)
+    return result
+
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(*data_structure)
+print(result)  # 99
