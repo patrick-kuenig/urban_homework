e04d5e3203a159883af9e1040503c54854e81e40
patrick-kuenig <kuenig.patrick@gmail.com>
2024-06-01 21:25:55 +0300

    forgot annotations

--- Module_5/module_5_1.py
+++ Module_5/module_5_1.py
@@ -1,9 +1,9 @@
 class House:
-    def __init__(self, name, number_of_floors):
+    def __init__(self, name: str, number_of_floors: int):
         self.name = name
         self.number_of_floors = number_of_floors
 
-    def go_to(self, new_floor):
+    def go_to(self, new_floor: int):
         if 0 < new_floor <= self.number_of_floors:
             print(new_floor)
         else:
