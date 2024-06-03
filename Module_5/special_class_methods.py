63b57690f95b715ff9c457c7a6248cadad14fbea
patrick-kuenig <kuenig.patrick@gmail.com>
2024-06-03 14:41:51 +0300

    homework_03_06_2024

---
+++ Module_5/special_class_methods.py
@@ -0,0 +1,7 @@
+class House:
+    def __init__(self):
+        self.numberOfFloors = 0
+
+    def setNewNumberOfFloors(self, floors):
+        self.numberOfFloors = floors
+        print(self.numberOfFloors)
