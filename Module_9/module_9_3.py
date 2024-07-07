dd98942496565e46b618fa79162caa8219fd9403
patrick-kuenig <kuenig.patrick@gmail.com>
2024-07-07 23:08:55 +0300

    homework_07_07_2024

---
+++ Module_9/module_9_3.py
@@ -0,0 +1,13 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (abs(len(first_word) - len(second_word)) for (first_word, second_word) in zip(first, second)
+                if not len(first_word) == len(second_word))
+
+# second_result = (first[index] == second[index] for index in range(max(len(first), len(second))))
+# [False, False, False]
+# В задании запрашивается "результат сравнения строк", не сразу понятно, что надо сравнить именно длину строк
+second_result = (len(first[index]) == len(second[index]) for index in range(max(len(first), len(second))))
+
+print(list(first_result))
+print(list(second_result))
