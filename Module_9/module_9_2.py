075599ba241544f780df95b5782320e69c796b38
patrick-kuenig <kuenig.patrick@gmail.com>
2024-07-07 22:23:20 +0300

    homework_07_07_2024

--- Module_9/module_9_2.py
+++ Module_9/module_9_2.py
@@ -3,6 +3,6 @@ second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler
 
 first_result = [len(name) for name in first_strings if len(name) > 5]
 second_result = [(name, word) for name in first_strings for word in second_strings if len(name) == len(word)]
-third_result = {key: len(key) for key in (first_strings + second_strings)}
+third_result = {key: len(key) for key in (first_strings + second_strings) if not len(key) % 2}
 
 print(first_result, second_result, third_result, sep='\n')
