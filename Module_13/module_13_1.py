502ae053e24b308d5cb9208c847c1babb7dd90d7
patrick-kuenig <kuenig.patrick@gmail.com>
2024-08-28 00:09:35 +0300

    homework_27_08_2024

--- Module_13/module_13_1.py
+++ Module_13/module_13_1.py
@@ -1,20 +1,24 @@
 import asyncio
 import random
 
+
 async def start_strongman(name, power):
     print(f'Силач {name} начал соревнования')
     for ball in range(1, 6):
-        await asyncio.sleep(1/power)
+        await asyncio.sleep(1 / power)
         print(f'Силач {name} поднял шар №{ball}')
     print(f'Силач {name} закончил соревнования.')
 
+
 async def start_tournament():
     names = ["Андрей", "Александр", "Максим", "Олег", "Егор", "Степан", "Владислав", "Вячеслав", "Боб"]
-    task1 = asyncio.create_task(start_strongman(names[random.randint(0, len(names) - 1)], random.random()))
-    task2 = asyncio.create_task(start_strongman(names[random.randint(0, len(names) - 1)], random.random()))
-    task3 = asyncio.create_task(start_strongman(names[random.randint(0, len(names) - 1)], random.random()))
-    await task1
-    await task2
-    await task3
+    tasks = []
+    for task in range(3):
+        task_ = asyncio.create_task(start_strongman(names[random.randint(0, len(names) - 1)], random.random()))
+        tasks.append(task_)
+    for task in tasks:
+        await task
 
-asyncio.run(start_tournament())
+
+if __name__ == '__main__':
+    asyncio.run(start_tournament())
