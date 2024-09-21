849dc887796b8b6e26b52025fa8eb1a1ee9cdbc9
patrick-kuenig <kuenig.patrick@gmail.com>
2024-09-21 16:08:30 +0300

    homework_06_09_2024

---
+++ Module_14/crud_functions.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+
+def initiate_db():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INT PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INT NOT NULL
+    )
+    ''')
+
+    # for i in range(5):
+    #     cursor.execute("""
+    #     INSERT INTO Products (title, description, price) VALUES (?, ?, ?)
+    #     """, (f'Product {i}', f'Product {i} is the greatest product ever', i*30))
+    #
+    # connection.commit()
+    # connection.close()
+
+def get_all_products():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Products')
+    all_products = cursor.fetchall()
+
+    connection.commit()
+    connection.close()
+
+    return all_products
+
+
+if __name__ == '__main__':
+    initiate_db()
