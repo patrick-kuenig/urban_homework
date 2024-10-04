dfec6052e2bece81fcac341336280123b69498f0
Patrick <kuenig.patrick@gmail.com>
2024-10-04 22:45:04 +0300

    homework_04_10_2024

--- Module_16/module_6_2.py
+++ Module_16/module_6_2.py
@@ -22,4 +22,4 @@ async def username_age(username: str = Path(min_length=5, max_length=20, descrip
 
 
 if __name__ == '__main__':
-    uvicorn.run('module_6_1:app', port=8000, reload=True)
+    uvicorn.run('module_6_2:app', port=8000, reload=True)
